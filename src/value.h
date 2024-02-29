4aefc68690034f70b1dd4db74ff953f562aa107c
Braden Mitchell <32005299+bradenmitchell@users.noreply.github.com>
2024-02-29 10:35:46 +1030

    feat: Chapter 18 "Types of Values"

--- src/value.h
+++ src/value.h
@@ -3,13 +3,41 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum ValueType {
+  VAL_BOOL,
+  VAL_NIL,
+  VAL_NUMBER,
+} ValueType;
+
+typedef struct Value {
+  ValueType type;
+  union {
+    bool boolean;
+    double number;
+  } as;
+} Value;
+
+// Type guards for Lox->C conversion
+#define IS_BOOL(value)   ((value).type == VAL_BOOL)
+#define IS_NIL(value)    ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+// Convert Lox value to C value
+#define AS_BOOL(value)   ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+// convert C value to Lox value
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
 typedef struct ValueArray {
   int capacity;
   int count;
   Value* values;
 } ValueArray;
+
+bool valuesEqual(Value a, Value b);
 
 void initValueArray(ValueArray* array);
 void freeValueArray(ValueArray* array);
